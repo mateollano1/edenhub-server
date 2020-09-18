bf7067a85947654bc97fb2e4bfe0f3fbde72b1cb
Mateo Llano <mateo.llano1@gmail.com>
2020-09-18 14:44:40 -0500

    dp updated

--- app/api/endpoints/product.js
+++ app/api/endpoints/product.js
@@ -81,7 +81,7 @@ router.post('', async(req, res) => {
 
 router.post('/bill', async(req, res) => {
     try {
-        deliveryPrice = 0
+        deliveryPrice = 3000
         finalPrice = 0
         console.log(req.body);
         let cartProduct = req.body
